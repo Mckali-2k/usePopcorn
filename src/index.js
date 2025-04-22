400001386da20e96db072c01fa71d30c4e4920d4
Mckali-2k <kalid538598@gmail.com>
2025-04-22 07:26:47 +0300

    added the star rating component
    the star components is a prop Api component that accepts deferent parameters like class-name, color, size, ...

--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import StarRating from './StarRating';
+/* import './index.css';
+import App from './App'; */
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <StarRating starNum={5} className={'test'} messages={['Terrible', 'bad', 'Okay', 'Good', 'Amazing']} defaultRating={4}/>
+    <StarRating starNum={6} color={'red'}/>
+    <StarRating size={25}/>
+    <TestRating />
+
+{/*     <App />
+ */}  </React.StrictMode>
 );
 
+function TestRating() {
+  const [movieRating, setMovieRating] = useState(0);
+
+  return (
+    <>
+        {<StarRating
+           starNum={5} 
+           className={'test'} 
+           messages={['Terrible', 'bad', 'Okay', 'Good', 'Amazing']} 
+           defaultRating={4}
+           setMovieRating={setMovieRating}
+          />
+        }
+        <p>This Movie was Rated {movieRating} </p>
+    </>
+
+  )
+}
